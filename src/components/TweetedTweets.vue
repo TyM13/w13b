af005e9ed4f160deedbe473b5ed03baf4e036399
Your Name <you@example.com>
2022-08-13 07:50:26 -0600

    w13b

--- src/components/TweetedTweets.vue
+++ src/components/TweetedTweets.vue
@@ -6,41 +6,33 @@
 </template>
 
 <script>
-
-
-export default {
-
-
-
-
+export default {  
   mounted() {
     this.show_tweets();
 
-let button_one = document.querySelector(`.button_one`)
-button_one.addEvenListener(`click`, this.page_orientation)
-  
+    let button_one = document.querySelector(`.button_one`);
+    button_one.addEvenListener(`click`, this.page_orientation);
   },
 
   methods: {
+        page_orientation() {
 
-//     page_orientation() {
-  
-// [`target`][`display`][`grid-auto-flow`] 
-
-//     },
+    [`display`][`grid-template-coloumns: 1fr 1fr 1fr`]
+ 
+        },
 
     show_tweets: function () {
       for (let i = 0; i < this.tweets.length; i++) {
         document.querySelector(`.tweet_container`).insertAdjacentHTML(
           `afterbegin`,
-          `<h3>Tweet:${this.tweets[i]}</h3>
-          <h1>user:${this.user[i]}</h1>
-          <h1>Date Created:${this.created[i]}</h1>`
+          `<h1>user:${this.user[i]}</h1>
+          <h2>Tweet:${this.tweets[i]}</h2>
+          <h4>Date Created:${this.created[i]}</h4>`
         );
       }
     },
   },
-  
+
   data() {
     return {
       tweets: [`randomtweet1`, `randomtweet2`, `randomtweet3`],
@@ -54,8 +46,9 @@ button_one.addEvenListener(`click`, this.page_orientation)
 </script>
 
 <style scoped>
-h1 {
+.tweet_container {
   display: grid;
-  grid-auto-flow: column;
+  grid-auto-flow: row ;
+  /* grid-template-columns: 1fr 1fr 1fr;   for reference*/
 }
 </style>
