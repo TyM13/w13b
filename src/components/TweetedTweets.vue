e0ef5e1939d7ea817b7f0c96a4af7c2ef7509710
Your Name <you@example.com>
2022-08-13 22:18:49 -0600

    finished conditional statment and commented code

--- src/components/TweetedTweets.vue
+++ src/components/TweetedTweets.vue
@@ -6,37 +6,39 @@
 </template>
 
 <script>
-export default {
-  mounted() {
-    this.show_tweets();
+export default {                                                          
+  mounted() {                                                             // when everything is loaded on the page
+    this.show_tweets();                                                   // display this(current component) show_tweets
 
-    let button_one = document.querySelector(`.button_one`);
-    button_one.addEventListener(`click`, this.page_orientation);
+    let button_one = document.querySelector(`.button_one`);               // gets the first button_one (class, button on page) sets to variable button_one
+    button_one.addEventListener(`click`, this.page_orientation);          // when button_one is clicked it will call the page orientation component
   },
 
   methods: {
-    page_orientation() {                                                          //still don't know how to write syntax proplerly
-      let tweet_container = document.getElementsByClassName(`tweet_container`);
-      if ([`style`][`grid-auto-flow: row` === true]) {
-        tweet_container[`style`][`grid-template-columns`] = `1fr 1fr 1fr`;             
-      } else if ([`style`][`grid-template-columns: 1fr 1fr 1fr;` === true]) {
-        tweet_container[`style`][`grid-auto-flow: row`];
+    page_orientation() {                                                  // function named page orientation
+      let tweet_container = document.querySelector(`.tweet_container`);   // gets the first tweet_container (class) and sets it as the variable tweet_container
+      if (tweet_container[`style`][`gridAutoFlow`] === `row`) {           // conditional that checks to see if tweet_container is equal to row
+        tweet_container[`style`][`gridAutoFlow`] = `column`;              // if its = to row it will set tweet_container to coloumn
+      } else {
+        tweet_container[`style`][`gridAutoFlow`] = `row`;                 // otherwise it will set the tweet_container to row
       }
     },
 
-    show_tweets: function () {
-      for (let i = 0; i < this.tweets.length; i++) {
-        document.querySelector(`.tweet_container`).insertAdjacentHTML(
+    show_tweets: function () {                                            // show_tweets function
+      for (let i = 0; i < this.tweets.length; i++) {                      // loops through the tweets if i is less than the tweets length it will incrament by 1
+        document.querySelector(`.tweet_container`).insertAdjacentHTML(    // gets the tweet_container from the page (class) and injects html from user i
           `afterbegin`,
-          `<h1>user:${this.user[i]}</h1>
+          `<section>
+          <h1>user:${this.user[i]}</h1>
           <h2>Tweet:${this.tweets[i]}</h2>
-          <h4>Date Created:${this.created[i]}</h4>`
+          <h4>Date Created:${this.created[i]}</h4>
+          </section>`
         );
       }
     },
   },
 
-  data() {
+  data() {                                                                // object containing different keys and values [array]
     return {
       tweets: [`randomtweet1`, `randomtweet2`, `randomtweet3`],
       user: [`Tyler`, `David`, `Sam`],
@@ -52,6 +54,5 @@ export default {
 .tweet_container {
   display: grid;
   grid-auto-flow: row;
-  /* grid-template-columns: 1fr 1fr 1fr;   for reference*/
 }
 </style>
